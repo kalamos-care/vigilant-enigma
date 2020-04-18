de2d82f659ed4d6d8a1b74d0f45500a362d6f57d
Gregory Roman <gregorymichaelroman@gmail.com>
2020-04-18 14:52:17 -0400

    Add gatsby plugin material ui.

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,18 @@ module.exports = {
         '~': path.join(__dirname, 'src/'),
       },
     },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      // If you want to use styled components, in conjunction to Material-UI, you should:
+      // - Change the injection order
+      // - Add the plugin
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      },
+      // 'gatsby-plugin-styled-components',
+    },
     // {
     //   resolve: `gatsby-plugin-google-analytics`,
     //   options: {
