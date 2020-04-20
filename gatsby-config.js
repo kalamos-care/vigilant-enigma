3073e0ce9e4a7a87bceea65f62572d1bfcb50274
Brad Thorson <bbthorson@gmail.com>
2020-04-20 14:27:27 -0400

    Update gatsby-config.js
    changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#67568c`,
         theme_color: `#67568c`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/K_Fav.png`, // This path is relative to the root of the site.
       },
     },
     {
