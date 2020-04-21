f079bde73b815694ca25542d63a598351131799a
Gregory Roman <gregorymichaelroman@gmail.com>
2020-04-20 22:42:42 -0400

    Update proximity function.

--- functions/proximity.js
+++ functions/proximity.js
@@ -1,8 +1,19 @@
-// https://docs.netlify.com/functions/build-with-javascript/#format
-exports.handler = function(event, context, callback) {
-  const  response = {
-    statusCode: 200,
-    body: "Hello, Kalamos Proximity Function."
-  };
-  callback(null, response);
-}
+const fetch = require('node-fetch').default;
+
+export async function handler(event) {
+  try {
+    const qs = event.queryStringParameters;
+    const query = Object.keys(qs).map(key => (`${key}=${qs[key]}`));
+    const response = await fetch(`https://npin.cdc.gov/api/organization/proximity?${query.join('&')}`)
+    const json = await response.json()
+    return {
+      statusCode: 200,
+      body: JSON.stringify(json),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: {}
+    }
+  }
+};
