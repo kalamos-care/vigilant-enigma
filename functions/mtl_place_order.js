a59de335b79c1641f5482332dde05d5cde21299d
Brad Thorson <bbthorson@gmail.com>
2020-04-23 08:24:21 -0400

    Update mtl_place_order.js
    print out all shipping info

--- functions/mtl_place_order.js
+++ functions/mtl_place_order.js
@@ -1,3 +1,6 @@
+const practice_id = "P04754";
+const physcian_id = 61745;
+
 exports.handler = function(event, context, callback) {
     const payload = JSON.parse(event.body); 
     callback(null, {
@@ -5,5 +8,13 @@ exports.handler = function(event, context, callback) {
         body: 'success',
     });
     // console.log(payload);
-    console.log(payload.shipping_address.first_name);    
+    console.log(payload.shipping_address.first_name); 
+    console.log(payload.shipping_address.last_name);
+    console.log(payload.shipping_address.address1);
+    console.log(payload.shipping_address.address2);
+    console.log(payload.shipping_address.city);
+    console.log(payload.shipping_address.zip);
+    console.log(payload.shipping_address.country);
+    console.log(practice_id);
+    console.log(physician_id);
 }
