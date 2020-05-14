c3d3a88ff44784b964ff45691e9efcdc097a86cb
Gregory Roman <gregorymichaelroman@gmail.com>
2020-05-13 21:37:39 -0400

    Add generic function for shopify mutations.

--- functions/lib/shopify.js
+++ functions/lib/shopify.js
@@ -1,3 +1,37 @@
+const mutationQueries = {
+  customerCreate: customer => ({
+    query: `mutation {
+      customerCreate(input: {
+        email: "${customer.email}",
+        firstName: "${customer.firstName}",
+        lastName: "${customer.lastName}",
+        password: "${customer.password}"
+      }) {
+        customer {
+          id
+        }
+      }
+    }`
+  }),
+  customerAccessTokenCreate: customer => ({
+    query: `mutation {
+      customerAccessTokenCreate(input: {
+        email: "${customer.email}",
+        password: "${customer.password}",
+      }) {
+        customerUserErrors {
+          code
+          field
+          message
+        }
+        customerAccessToken {
+          accessToken
+          expiresAt
+        }
+      }
+    }`
+  })
+};
 
 if (process.env.NETLIFY_DEV) {
   require('dotenv').config({ path: '.env.development' })
@@ -10,3 +44,5 @@ module.exports.headers = {
   'Content-Type': 'application/json',
   'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
 };
+
+module.exports.mutationQuery = mutation => mutationQueries[mutation];
--- functions/shopify_mutation.js
+++ functions/shopify_mutation.js
@@ -1,28 +1,17 @@
 const fetch = require('node-fetch').default;
-const { url, headers} = require('./lib/shopify');
-
-const customerCreateQuery = customer => ({
-  query: `mutation {
-    customerCreate(input: {
-      email: "${customer.email}",
-      firstName: "${customer.firstName}",
-      lastName: "${customer.lastName}",
-      password: "${customer.password}"
-    }) {
-      customer {
-        id
-      }
-    }
-  }`
-});
+const { url, headers, mutationQuery } = require('./lib/shopify');
 
 exports.handler = async (event) => {
   try {
     const body = JSON.parse(event.body);
+    const mutation = event.headers['x-shopify-mutation'];
+    if (typeof mutation !== 'string') {
+      throw('');
+    }
     const response = await fetch(url, {
       method: 'POST',
       headers,
-      body: JSON.stringify(customerCreateQuery(body)),
+      body: JSON.stringify(mutationQuery(mutation)(body)),
     })
     const json = await response.json()
     return {
@@ -35,7 +24,9 @@ exports.handler = async (event) => {
   } catch (error) {
     return {
       statusCode: 500,
-      body: {}
+      body: JSON.stringify({
+        message: 'An error occurred'
+      })
     }
   }
 };
